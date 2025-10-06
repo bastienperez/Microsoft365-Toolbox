296eed75914c23f3f545e31df81bc10bbb7c8876
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-10-06 12:08:14 +0200

    update function to get tenant ID

---
+++ Get-Microsoft365TenantID.ps1
@@ -0,0 +1,10 @@
+# Another great way to get tenant ID from domain: https://www.whatismytenantid.com/result
+function Get-Microsoft365TenantID {
+	param(
+		[CmdletBinding()]
+		[Parameter(Mandatory = $true, Position = 0)]
+		[string]$Domain
+	)
+
+	(Invoke-WebRequest https://login.windows.net/$Domain/.well-known/openid-configuration | ConvertFrom-Json).Token_Endpoint.Split('/')[3]
+}
