d6fcf90b95cea98006dceca0a14daeb775bd4d43
Bastien Perez <104252785+bastienperez@users.noreply.github.com>
2025-10-23 10:43:40 +0200

    rename scim app script

--- Applications/SCIM/Get-MgApplicationSCIM.ps1
+++ Applications/SCIM/Get-MgApplicationSCIM.ps1
@@ -1,24 +1,56 @@
-# The groups assignments are not retrieve because based on https://main.iam.ad.ext.azure.com
 <#
-Version History:
+.SYNOPSIS
+    Retrieves all Entra ID applications configured for SCIM provisioning.
 
-## [1.2] - 2025-05-27
-### Added
-- Export functionality for synchronization job details
-- Support for additional synchronization job properties
+.DESCRIPTION
+    This function returns a list of all Entra ID applications with SCIM provisioning enabled,
+    along with their synchronization job details and settings.
 
-## [1.1] - 2025-02-26
-### Changed
-- Transform the script into a function
-- Replace `Write-Host` with `Write-Verbose`
+.EXAMPLE
+    $scimApps = Get-MgApplicationSCIM
 
-## [1.0] - 2024-xx-xx
-### Initial Release
+    Retrieves all Entra ID applications with SCIM provisioning enabled.
+
+.EXAMPLE
+    Get-MgApplicationSCIM -ForceNewToken
+
+    Forces the function to disconnect and reconnect to Microsoft Graph to obtain a new access token.
+
+    .EXAMPLE
+    Get-MgApplicationSCIM -Export
+
+    Exports the SCIM configuration details to a CSV file.
+
+    .EXAMPLE
+    Get-MgApplicationSCIM -ObjectID "xxx-xxx-xxx"
+
+    Retrieves the SCIM configuration for a specific application by its ObjectID.
+
+    .NOTES
+    .REQUIREMENTS
+    This function requires the Microsoft.Graph.Applications and Microsoft.Graph.Authentication modules.
+
+    .AUTHOR
+    Bastien Perez
+
+    .LIMITATIONS
+    The groups assignments are not retrieved because based on https://main.iam.ad.ext.azure.com
+
+    .CHANGELOG
+    ### Added
+    - Export functionality for synchronization job details
+    - Support for additional synchronization job properties
+
+    ## [1.1] - 2025-02-26
+    ### Changed
+    - Transform the script into a function
+    - Replace `Write-Host` with `Write-Verbose`
+
+    ## [1.0] - 2024-xx-xx
+    ### Initial Release
 #>
 
-## Modules : Microsoft.Graph.Applications & Microsoft.Graph.Authentication
-
-function Get-MgSCIMConfiguration {
+function Get-MgApplicationSCIM {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $false)]
@@ -29,8 +61,8 @@ function Get-MgSCIMConfiguration {
         [switch]$Export
     )
 
-    [System.Collections.Generic.List[PSObject]]$synchronizationJobsArray = @()
-    [System.Collections.Generic.List[PSObject]]$synchronizationJobsDetailsArray = @()
+    [System.Collections.Generic.List[PSCustomObject]]$synchronizationJobsArray = @()
+    [System.Collections.Generic.List[PSCustomObject]]$synchronizationJobsDetailsArray = @()
 
     if ($ForceNewToken.IsPresent) {
         if (Get-MgContext) {
@@ -87,7 +119,7 @@ function Get-MgSCIMConfiguration {
             $job | Add-Member -MemberType NoteProperty -Name TroubleshootingUrl -Value $job.Status.TroubleshootingUrl
 
             # synchronizationJobSettings Value
-            foreach($property in $job.SynchronizationJobSettings) {
+            foreach ($property in $job.SynchronizationJobSettings) {
                 $job | Add-Member -MemberType NoteProperty -Name $property.Name -Value $property.Value
             }
 
@@ -109,17 +141,16 @@ function Get-MgSCIMConfiguration {
         Write-Host "Get synchronization settings $($job.DisplayName) ($j/$($synchronizationJobsArray.Count))"
 
         $jobSchema = Get-MgServicePrincipalSynchronizationJobSchema -ServicePrincipalId $job.ServicePrincipalId -SynchronizationJobId $job.Id
-   
+
         $job | Add-Member -MemberType NoteProperty -Name Scheduling -Value $job.Schedule.Interval
         $job | Add-Member -MemberType NoteProperty -Name SchedulingState -Value $job.Schedule.State
         $job | Add-Member -MemberType NoteProperty -Name LastSuccessfulExecutionDate -Value $job.Status.LastSuccessfulExecution.TimeEnded
         $job | Add-Member -MemberType NoteProperty -Name LastSuccessfulExecutionState -Value $job.Status.LastSuccessfulExecution.State
         $job | Add-Member -MemberType NoteProperty -Name LastSuccessfulExecutionWithExportsDate -Value $job.Status.LastSuccessfulExecutionWithExports.TimeEnded
         $job | Add-Member -MemberType NoteProperty -Name LastSuccessfulExecutionWithExportsState -Value $job.Status.LastSuccessfulExecutionWithExports.State
-    
+
         if ($job.Status.Quarantine.CurrentBegan) {
             $job | Add-Member -MemberType NoteProperty -Name Quarantined -Value $true
-
         }
         else {
             $job | Add-Member -MemberType NoteProperty -Name Quarantined -Value $false
@@ -133,7 +164,7 @@ function Get-MgSCIMConfiguration {
             $job | Add-Member -MemberType NoteProperty -Name "SynchronizedEntryCountByType_$key" -Value $count
         }
 
-        [System.Collections.Generic.List[PSObject]]$attributesArray = @()
+        [System.Collections.Generic.List[PSCustomObject]]$attributesArray = @()
     
         foreach ($objectMapping in $jobSchema.SynchronizationRules.ObjectMappings) {
 
@@ -143,16 +174,6 @@ function Get-MgSCIMConfiguration {
                 $targetObjectType = $mapping.TargetObjectName
 
                 foreach ($attribute in $mapping.AttributeMappings) {
-        
-                    <#
-                    DefaultValue            :
-                    ExportMissingReferences : False
-                    FlowBehavior            : FlowWhenChanged
-                    FlowType                : Always
-                    MatchingPriority        : 0
-                    Source                  : Microsoft.Graph.PowerShell.Models.MicrosoftGraphAttributeMappingSource
-                    TargetAttributeName     : members
-                #>
                 
                     $object = [PSCustomObject][ordered]@{
                         Type                    = $targetObjectType
